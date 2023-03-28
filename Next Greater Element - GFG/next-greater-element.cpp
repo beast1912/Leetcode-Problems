bbb24e304677dabbe1661a57cc9af08b1ac82afc
Uday Sanghani <72157802+beast1912@users.noreply.github.com>
2023-03-28 18:50:37 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -10,27 +10,30 @@ class Solution
     //Function to find the next greater element for each element of the array.
     vector<long long> nextLargerElement(vector<long long> arr, int n){
         // Your code here
+        stack<long long>s;
         vector<long long> ans;
-        stack<long long> s;
         for(int i = n-1; i>=0; i--)
         {
             if(s.empty())
                 ans.push_back(-1);
-            else if(s.top()> arr[i])
+            else if(s.top() > arr[i])
                 ans.push_back(s.top());
             else if(s.top() <= arr[i])
             {
-                while(!s.empty() && s.top() <= arr[i])
+                while(!s.empty() && s.top()<=arr[i])
+                {
                     s.pop();
+                }
                 if(s.empty())
                     ans.push_back(-1);
-                else if(s.top() > arr[i])
+                else
                     ans.push_back(s.top());
             }
             s.push(arr[i]);
         }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
+        
         
     }
 };
